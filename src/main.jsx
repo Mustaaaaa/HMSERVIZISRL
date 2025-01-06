714c5e0cbc6748bb9a7e862f197e6ec06ce8fc17
Mustaaaaa <moustafaibrahim1512@gmail.com>
2025-01-06 23:57:43 +0100

    add: aggiunto il setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import './app.css'
+import Header from './Header/Header'
+import MainContent from './MainContent/MainContent'
+import Footer from './Footer/Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+    <MainContent />
+    <Footer />
   </StrictMode>,
 )
