9b46c12d4f1392aef9af047f65bfe3f271cbf2b9
Mustaaaaa <moustafaibrahim1512@gmail.com>
2025-04-22 01:31:13 +0200

    add: aggiunto il router per le pagine

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // <-- importa questo
 import './index.css'
 import './app.css'
 import Header from './Header/Header'
 import MainContent from './MainContent/MainContent'
-import Footer from './Footer/Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
-    <MainContent />
-    <Footer />
-  </StrictMode>,
+    <BrowserRouter>
+      <Header />
+      <MainContent />
+    </BrowserRouter>
+  </StrictMode>
 )
