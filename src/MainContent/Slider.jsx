8b3380692014e65f2f5fc21656f4f5681a85eea2
Mustaaaaa <moustafaibrahim1512@gmail.com>
2025-04-22 01:31:30 +0200

    add: creazione di uno slider per la home

---
+++ src/MainContent/Slider.jsx
@@ -0,0 +1,38 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const Slider = () => {
+  const images = [
+    "/public/foto1.webp",
+    "/public/screenshot.png",
+  ];
+
+  return (
+    <div className="h-full w-full mx-auto flex">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={20}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        loop
+        className="rounded-lg shadow-lg"
+      >
+        {images.map((img, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={img}
+              alt={`Slide ${index + 1}`}
+              className="w-full h-full object-cover rounded-lg"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Slider;
