5cfc0af1e6bbafd75b5b90d9719a16e5f620fe8a
Mustaaaaa <moustafaibrahim1512@gmail.com>
2025-04-22 01:39:20 +0200

    add: nome per app.cc sistemato

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // <-- importa questo
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import './app.css'
+import './App.css'
 import Header from './Header/Header'
 import MainContent from './MainContent/MainContent'
 
