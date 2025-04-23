7337c8a2bee66f79e679fde7006d4bca0f15c922
Mustaaaaa <moustafaibrahim1512@gmail.com>
2025-04-23 23:13:38 +0200

    Add: reso il footer responsive

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -5,52 +5,64 @@ import Logo from '/logotras.png'
 const Footer = () => {
   return (
     <div>
-      <div className="flex h-[calc(350px)] justify-center  gap-10 p-20">
-        <div className='w-1/6'>
-          <img src={Logo} alt="" className='w-52' />
+      {/* Contenuto principale del footer */}
+      <div className="flex flex-col md:flex-row justify-between items-center gap-10 pt-10">
+        {/* Logo */}
+        <div className="w-2/5 lg:w-1/6 flex justify-center">
+          <img src={Logo} alt="Logo" className="w-52" />
         </div>
 
-        <div className='w-1/6 flex flex-col gap-6'>
-          <p className="text-2xl font-semibold">Sede legale</p>
+        <div className='flex flex-wrap w-2/5 lg:w-4/6 justify-center gap-y-10'>
 
-          <div>
-            <p>Via Collodi, N.2</p>
-            <p>24047 Treviglio(BG)</p>
+          {/* Sede legale */}
+          <div className="w-full lg:w-1/3 flex flex-col gap-6 text-center lg:text-start">
+            <p className="text-2xl font-semibold">Sede legale</p>
+            <div>
+              <p>Via Collodi, N.2</p>
+              <p>24047 Treviglio(BG)</p>
+            </div>
+          </div>
+
+          {/* Social */}
+          <div className="w-full lg:w-1/3 flex flex-col gap-6 text-center lg:text-start">
+            <p className="text-2xl font-semibold">Social</p>
+            <div className="flex flex-col gap-2">
+              <a href="/" className="hover:underline">Facebook</a>
+              <a href="/" className="hover:underline">LinkedIn</a>
+            </div>
+          </div>
+
+          {/* Informazioni */}
+          <div className="w-full sm:w-full lg:w-1/3 flex flex-col gap-6 text-center lg:text-start">
+            <p className="text-2xl font-semibold">Informazioni</p>
+            <p>Per informazioni, domande o riconoscimenti, chiama il numero</p>
           </div>
 
         </div>
 
-        <div className='w-1/6 flex flex-col gap-6'>
-          <p className="text-2xl font-semibold">Social</p>
-          <div className='flex flex-col'>
-            <a href="/">Facebook</a>
-            <a href="/">LinkedIn</a>
-          </div>
-        </div>
 
-        <div className='w-1/6 flex flex-col gap-6'>
-          <p className="text-2xl font-semibold">Informazioni</p>
-          <p>Per informazioni, domande o riconoscimenti, chiama il numero </p>
-        </div>
 
-        <div className='w-1/6 flex justify-center'>
+        {/* Bottone Contattaci */}
+        <div className="w-1/5 lg:w-1/6 flex justify-center">
           <a
-            href='/Contatti'
-            className='w-52 h-16 bg-blue-700 text-white font-bold rounded-md hover:bg-white hover:text-blue-700 border-2 border-blue-700 onclick:bg-black transition duration-500 active:bg-gray-300 flex items-center justify-center'>
+            href="/Contatti"
+            className="w-full md:w-52 h-14 bg-blue-700 text-white font-bold rounded-md hover:bg-white hover:text-blue-700 border-2 border-blue-700 transition duration-500 active:bg-gray-300 flex justify-center items-center"
+          >
             Contattaci
           </a>
         </div>
       </div>
 
-      <div className='flex justify-between px-28'>
-        <p>© 2024 by H.M SERVIZI</p>
-
-        <div className='flex gap-7'>
-          <a href="/">Informativa sui cookie</a>
-          <a href="/">Informativa sulla privacy</a>
+      {/* Footer inferiore (copyright e link informativi) */}
+      <div className="flex flex-col md:flex-row justify-between items-center py-14">
+        <p className="text-center md:text-left">© 2024 by H.M SERVIZI</p>
+        <div className="flex gap-7 text-center md:text-left">
+          <a href="/" className="hover:underline">Informativa sui cookie</a>
+          <a href="/" className="hover:underline">Informativa sulla privacy</a>
         </div>
       </div>
     </div>
+
   )
 }
 
