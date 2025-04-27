03ac34f76411ba73c7a02989d56f592a16d83598
Mustaaaaa <moustafaibrahim1512@gmail.com>
2025-04-28 00:44:11 +0200

    add: creata la versione mobile

--- src/MainContent/Contatti.jsx
+++ src/MainContent/Contatti.jsx
@@ -59,7 +59,7 @@ const Contatti = () => {
           title: "Inviato con successo!",
           icon: "success",
           confirmButtonText: 'Chiudi',
-          confirmButtonColor: '#F97316',
+          confirmButtonColor: '#034792',
           customClass: {
             confirmButton: 'rounded-full'
           }
@@ -72,7 +72,7 @@ const Contatti = () => {
           text: 'Riprova a compilare il form e rinviarlo!',
           icon: 'error',
           confirmButtonText: 'Chiudi',
-          confirmButtonColor: '#F97316',
+          confirmButtonColor: '#034792',
           customClass: {
             confirmButton: 'rounded-full'
           }
@@ -84,7 +84,7 @@ const Contatti = () => {
         text: 'Si è verificato un problema con la connessione. Riprova più tardi.',
         icon: 'error',
         confirmButtonText: 'Chiudi',
-        confirmButtonColor: '#F97316',
+        confirmButtonColor: '#034792',
         customClass: {
           confirmButton: 'rounded-full'
         }
@@ -95,61 +95,66 @@ const Contatti = () => {
   return (
     <div>
 
-      <div className='flex flex-col lg:flex-row justify-center items-center lg:h-full px-28'>
+      <div className='flex flex-col lg:flex-row justify-center items-center lg:h-full md:px-28 px-6'>
 
-        <div className="flex flex-col h-[calc(80vh)] lg:h-auto justify-center items-center w-full mt-20 lg:mt-0 lg:items-start">
+        <div className="flex flex-col min-h-[calc(80vh)] md:w-1/2 lg:h-auto justify-center items-center pt-32 w-full lg:mt-0 lg:items-start">
 
-          <div className="pt-52">
-            <div className=''>
-              <p className='font-semibold text-xl'>Contattaci</p>
-              <div className='flex mt-4'>
-                <div className=''>
-                  <p className='title-section'>Per informazioni e domande</p>
-                </div>
+          <div>
+            <p className="font-semibold text-xl text-center md:text-left">Contattaci</p>
+            <div className="flex flex-col md:flex-row mt-4">
+              <div className="w-full max-w-[calc(850px)]">
+
+                <p className="text-3xl md:text-5xl text-center md:text-start font-semibold pt-5 pb-6 text-blue-700 max-w-[850px]">
+                  Per informazioni e domande
+                </p>
+
+                <p className='w-full md:w-2/3 pt-5 text-center md:text-start'>
+                  Usa il modulo di contatto o chiamaci al numero ....
+                </p>
+
               </div>
-              <p className='w-2/3 pt-5'> Usa il modulo di contatto o chiamaci al numero ....</p>
             </div>
           </div>
 
-          <form onSubmit={onSubmit} noValidate className="pt-14">
+          <form onSubmit={onSubmit} noValidate className="pt-14 w-2/3 md:w-1/2">
 
-            <div className="mb-4 gap-10 flex justify-between">
+            <div className="md:mb-4 gap-10 md:flex justify-between">
               <div>
-                <p>Nome</p>
-                <input name="name" type="text" placeholder="" className=" h-10 w-full border-b-2 border-blue-700 focus:outline-none focus:ring-0 focus:border-blue-500" />
+                <p className="text-blue-700">Nome</p>
+                <input name="name" type="text" placeholder="" className="h-10 w-full border-b-2 border-blue-700 focus:outline-none focus:ring-0 focus:border-blue-500" />
                 {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name}</p>}
               </div>
               <div>
-                <p>Cognome</p>
-                <input name="lastname" type="text" placeholder=" " className=" h-10 w-full border-b-2 border-blue-700 focus:outline-none focus:ring-0 focus:border-blue-500" />
+                <p className="text-blue-700 pt-4 md:pt-0">Cognome</p>
+                <input name="lastname" type="text" placeholder=" " className="h-10 w-full border-b-2 border-blue-700 focus:outline-none focus:ring-0 focus:border-blue-500" />
                 {errors.lastname && <p className="text-red-500 text-sm mt-1">{errors.lastname}</p>}
               </div>
             </div>
 
-            <div className="mb-4 gap-10 flex justify-between">
+            <div className="md:mb-4 gap-10 md:flex justify-between">
               <div>
-                <p>Email</p>
+                <p className="text-blue-700 pt-4 md:pt-0">Email</p>
                 <input name="email" type="email" placeholder=" " className="h-10 w-full  border-b-2 border-blue-700 focus:outline-none focus:ring-0 focus:border-blue-500" />
                 {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
               </div>
               <div>
-                <p>Cellulare</p>
+                <p className="text-blue-700 pt-4 md:pt-0">Cellulare</p>
                 <input name="number" type="tel" maxLength="10" placeholder="" className="h-10 w-full border-b-2 hover:border-b-4 border-blue-700 focus:outline-none focus:ring-0 focus:border-blue-500" />
                 {errors.number && <p className="text-red-500 text-sm mt-1">{errors.number}</p>}
               </div>
             </div>
 
             <div className="mb-4">
-              <p className="pb-3">Scrivi un messaggio...</p>
+              <p className="pb-3 text-blue-700 pt-4 md:pt-0">Scrivi un messaggio...</p>
               <textarea name="message" placeholder="" className="pl-2 pt-2 h-40 w-full border-2 border-blue-700 resize-none focus:outline-none focus:ring-0 focus:border-blue-500"></textarea>
               {errors.message && <p className="text-red-500 text-sm mt-1">{errors.message}</p>}
             </div>
-            <button type="submit" className="w-52 h-16 bg-blue-700 text-white font-bold rounded-md hover:bg-white hover:text-blue-700 border-2 border-blue-700 onclick:bg-black transition duration-500 active:bg-gray-300">Invia</button>
+            <button type="submit" className="w-full md:w-52 h-16 bg-blue-700 text-white font-bold rounded-md hover:bg-white hover:text-blue-700 border-2 border-blue-700 onclick:bg-black transition duration-500 active:bg-gray-300">Invia</button>
           </form>
         </div>
 
-        <div className='flex justify-center items-center pt-48'>
-          <img src={Foto1} alt="" className='aspect-[calc(1.5*3)/3] object-cover' />
+        <div className="w-full md:w-1/2 flex justify-center items-center pt-20">
+          <img src={Foto1} alt="Servizi operativi" className="aspect-[1.5] object-cover rounded-2xl shadow-md w-full max-w-[600px] md:max-w-none" />
         </div>
 
       </div>
